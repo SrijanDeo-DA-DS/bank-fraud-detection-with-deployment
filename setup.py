b4a7fb3593dacd14c57c01136354aca573691977
SrijanDeo-DA-DS <sd759@njit.edu>
2024-10-24 16:09:55 -0500

    add project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The Bank Fraud Detection ML project aims to identify and prevent fraudulent transactions in real-time by leveraging machine learning algorithms. The project involves building predictive models to detect anomalous patterns in financial transactions, distinguishing between legitimate and fraudulent activities',
+    author='SrijanDeo',
+    license='',
+)
